1a7853a0ba30ca8d767cb635f51320ad40415442
victor gamez royo <victorin_222@hotmail.com>
2015-10-02 18:52:43 +0200

    https://github.com/VGamezz19/victorvisual.git

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SumaBtn = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RestaBtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'SumaBtn
+        '
+        Me.SumaBtn.Location = New System.Drawing.Point(485, 78)
+        Me.SumaBtn.Name = "SumaBtn"
+        Me.SumaBtn.Size = New System.Drawing.Size(90, 32)
+        Me.SumaBtn.TabIndex = 0
+        Me.SumaBtn.Text = "suma"
+        Me.SumaBtn.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(219, 85)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(46, 85)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(371, 92)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(10, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = " "
+        '
+        'RestaBtn
+        '
+        Me.RestaBtn.Location = New System.Drawing.Point(485, 132)
+        Me.RestaBtn.Name = "RestaBtn"
+        Me.RestaBtn.Size = New System.Drawing.Size(90, 29)
+        Me.RestaBtn.TabIndex = 4
+        Me.RestaBtn.Text = "resta"
+        Me.RestaBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(623, 201)
+        Me.Controls.Add(Me.RestaBtn)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.SumaBtn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SumaBtn As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RestaBtn As Button
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    Dim op1 As Integer
+    Dim op2 As Integer
+    Dim result As Integer
+
+    Private Sub sumabtm_Click(sender As Object, e As EventArgs) Handles SumaBtn.Click
+
+        op1 = TextBox1.Text       'El op1 es un Integer (numero entero) y TextBox1 es un text. CInt hace la conversion sola para que cumpla la igualdad.  Ejemplo: = CInt(TextBox1.Text)
+        op2 = TextBox2.Text       'He quitado el CInt porque VS ya hace la conversion, pero en otros lenguajes no lo hace.
+
+        result = op1 + op2
+
+        Label1.Text = result.ToString() 'ToString hace la misma funcion que el CInt pero alrebes. De entero a texto.
+    End Sub
+
+    Private Sub RestaBtn_Click(sender As Object, e As EventArgs) Handles RestaBtn.Click
+
+        op1 = TextBox1.Text
+        op2 = TextBox2.Text
+
+        result = op1 - op2
+
+        Label1.Text = result.ToString()
+    End Sub
+End Class
